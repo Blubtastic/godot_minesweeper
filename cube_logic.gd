2b302a0ee6210aea5bf6387edef1c3fa1c519067
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2024-10-07 09:00:03 +0200

    cubegrid with three random colors

--- cube_logic.gd
+++ cube_logic.gd
@@ -1,4 +1,7 @@
 extends StaticBody3D
+@export var mesh1: Mesh
+@export var mesh2: Mesh
+@export var mesh3: Mesh
 
 var color1 = Color(1, 0, 0)
 var color2 = Color(0, 1, 0)
@@ -6,4 +9,5 @@ var color3 = Color(0, 0, 1, 0)
 var selectedColor = [color1, color2, color3].pick_random()
 
 func _ready() -> void:
-	$MeshInstance3D.mesh.material.albedo_color = selectedColor
+	var selectedMesh: Mesh = [mesh1, mesh2, mesh3].pick_random()
+	$MeshInstance3D.mesh = selectedMesh
