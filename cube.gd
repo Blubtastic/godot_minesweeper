4597a14a43f9cfc84233a10a67614f8613e8cc71
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2024-10-26 11:17:19 +0200

    optimize cube script

--- cube.gd
+++ cube.gd
@@ -5,9 +5,9 @@ extends StaticBody3D
 @export var bombMesh: Mesh
 @export var radioactiveMesh: Mesh
 
+@export var is_bomb: int = false
 var is_cleared: bool = false
 var is_flagged: bool = false
-@export var is_bomb: int = false
 var bomb_probability = 0.16
 
 var simulate_gravity = false
@@ -20,7 +20,6 @@ signal cube_was_cleared
 func _ready() -> void:
 	# is_bomb = true if (randi() % 100) < (bomb_probability*100) else false
 	var selectedMesh = mesh1
-	# duplicate meshes and materials 
 	$MeshInstance3D.mesh = selectedMesh.duplicate()
 	$MeshInstance3D.mesh.material = $MeshInstance3D.get_active_material(0).duplicate()
 
@@ -30,35 +29,31 @@ func _physics_process(delta):
 		translate(velocity * delta)
 
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-		if event is InputEventMouseButton:
-			var mouse_event := event as InputEventMouseButton
-			if !is_cleared:
-				if event.is_command_or_control_pressed() and mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-					toggle_flag()
-				elif mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-					if !is_flagged:
-						unhighlight_cube()
-						reveal_cube()
-						if is_bomb:
-							animateExplosion()
-				elif mouse_event.button_index == MOUSE_BUTTON_RIGHT and mouse_event.pressed:
-					toggle_flag()
-			else:
-				if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
-					if $Area3D.can_auto_clear:
-						for cube in $Area3D.cubes:
-							cube.reveal_cube()
+	if event is InputEventMouseButton:
+		var mouse_event := event as InputEventMouseButton
+		var isLeftClick = mouse_event.pressed and mouse_event.button_index == MOUSE_BUTTON_LEFT
+		var isRightLick = mouse_event.pressed and mouse_event.button_index == MOUSE_BUTTON_RIGHT
+		if !is_cleared:
+			if isRightLick or (isLeftClick and event.is_command_or_control_pressed()):
+				toggle_flag()
+			elif isLeftClick and !is_flagged:
+				unhighlight_cube()
+				reveal_cube()
+		elif isLeftClick and $Area3D.can_auto_clear:
+			for cube in $Area3D.cubes:
+				cube.reveal_cube()
 
 func reveal_cube():
 	if !is_cleared and !is_flagged:
-		var newMesh = bombMesh if is_bomb else flatMesh
-		$MeshInstance3D.mesh = newMesh
+		$MeshInstance3D.mesh = bombMesh if is_bomb else flatMesh
 		transform = transform.translated(Vector3(0, -0.1, 0))
 		$Label3D.visible = true
 		is_cleared = true;
 		cube_was_cleared.emit(self)
 		if is_bomb:
 			game_over.emit()
+			animateExplosion()
+
 
 func toggle_flag():
 	is_flagged = !is_flagged
--- scan_nearby.gd
+++ scan_nearby.gd
@@ -2,21 +2,21 @@ extends Area3D
 const colors = [
 	Color(0, 0, 1),
 	Color(0, 0.5, 0),
-	Color(1,0,0),
-	Color(0,0,0.5),
+	Color(1, 0, 0),
+	Color(0, 0, 0.5),
 	Color(0.3, 0.05, 0),
-	Color(0,0.4,0.5),
-	Color(0,0,0),
-	Color(0.5,0.5,0.5)
+	Color(0, 0.4, 0.5),
+	Color(0, 0, 0),
+	Color(0.5, 0.5, 0.5)
 ]
 
 var cubes
 var can_auto_clear = false
 
-func get_nearby_cube_data(cubes: Array[Node3D]):
+func get_nearby_cube_data(nearbyCubes: Array[Node3D]):
 	var bombCount = 0
 	var flagCount = 0
-	for cube in cubes:
+	for cube in nearbyCubes:
 		if cube.is_bomb:
 			bombCount += 1
 		if cube.is_flagged:
@@ -31,18 +31,18 @@ func _physics_process(_delta: float) -> void:
 	can_auto_clear = true if nearbyMines == nearbyFlags else false
 	var nearbyMinesNumber = str(nearbyMines) if nearbyMines > 0 else ''
 	
-	# TODO: optimize: all surrounding cube labels are refreshed every frame.
+	# TODO: optimize, all surrounding cube labels are refreshed every frame.
 	if $"..".is_cleared:
 		if nearbyMines == 0:
 			for cube in cubes:
 				cube.reveal_cube()
 		if $"..".is_bomb:
 			$"../Label3D".text = '💣'
-			$"../Label3D".modulate = Color(1,1,1)
+			$"../Label3D".modulate = Color(1, 1, 1)
 			for cube in cubes:
 				cube.reveal_cube()
 		else:
 			$"../Label3D".text = nearbyMinesNumber
-			var index = nearbyMines - 1 if nearbyMines < colors.size()-1 else 0
+			var index = nearbyMines - 1 if nearbyMines < colors.size() - 1 else 0
 			$"../Label3D".modulate = colors[index]
 			$"../Label3D".outline_modulate = colors[index]
