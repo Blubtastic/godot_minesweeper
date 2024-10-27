25e9a25458326cd3c027dce4bf50165a732aa099
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2024-10-27 14:17:00 +0100

    finish cleaning up cube_scanner

--- scripts/cube_scanner.gd
+++ scripts/cube_scanner.gd
@@ -18,27 +18,25 @@ func _ready() -> void:
 	Cube = $".."
 	NearbyMinesLabel = $"../NearbyMinesLabel"
 
+func update_cube() -> void:
+	overlapping_cubes = get_overlapping_bodies()
+	var nearby_items: Array[int] = get_nearby_cube_info(overlapping_cubes)
+	can_auto_clear = nearby_items[0] == nearby_items[1]
+	var nearby_mines: int = nearby_items[0]
+	if Cube.is_cleared:
+		var nearby_mines_text = str(nearby_mines) if nearby_mines else ''
+		var nearby_mines_color = COLORS[clamp(nearby_mines, 1, COLORS.size()) - 1]
+		var text = '💣' if Cube.is_bomb else nearby_mines_text
+		var color = Color(1, 1, 1) if Cube.is_bomb else nearby_mines_color
+		update_label(text, color)
+		if !nearby_mines:
+			for overlapping_cube in overlapping_cubes:
+				overlapping_cube.reveal_cube()
+
 func get_nearby_cube_info(nearbyCubes: Array[Node3D]) -> Array[int]:
 	var bombs := nearbyCubes.filter(func(nearbyCube): return nearbyCube.is_bomb)
 	var flags := nearbyCubes.filter(func(nearbyCube): return nearbyCube.is_flagged)
 	return [bombs.size(), flags.size()]
-
-func update_cube() -> void:
-	overlapping_cubes = get_overlapping_bodies()
-	var nearby_items := get_nearby_cube_info(overlapping_cubes)
-	can_auto_clear = nearby_items[0] == nearby_items[1]
-	var nearbyMines := nearby_items[0]
-	if Cube.is_cleared:
-		if Cube.is_bomb:
-			update_label('💣', Color(1, 1, 1))
-		else:
-			update_label(
-				str(nearbyMines) if nearbyMines else '',
-				COLORS[clamp(nearbyMines, 1, COLORS.size()) - 1]
-			)
-		if !nearbyMines:
-			for overlapping_cube in overlapping_cubes:
-				overlapping_cube.reveal_cube()
 
 func update_label(text: String, color: Color) -> void:
 	NearbyMinesLabel.text = text
