720eee61dda1b5be90404924e34c61fc50265636
Blubtastic <17567077+Blubtastic@users.noreply.github.com>
2024-11-19 21:13:38 +0100

    load cubes on game start

--- shared/cube/explosion_effect.gd
+++ shared/cube/explosion_effect.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@onready var debris: GPUParticles3D = $Debris
-@onready var smoke: GPUParticles3D = $Smoke
-@onready var fire: GPUParticles3D = $Fire
+@onready var debris: CPUParticles3D = $Debris
+@onready var smoke: CPUParticles3D = $Smoke
+@onready var fire: CPUParticles3D = $Fire
 
 func _ready():
 	explode()
